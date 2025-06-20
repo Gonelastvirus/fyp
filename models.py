ae8765bd0f12fbfb76050b4ee5ae8969cd52ba92
Gonelastvirus <67478827+Gonelastvirus@users.noreply.github.com>
2025-06-21 00:39:20 +0545

    Update models.py
    added password change admin features

--- models.py
+++ models.py
@@ -24,6 +24,15 @@ class User(UserMixin, db.Model):
     proposals = db.relationship('Proposal', backref='expert', lazy='dynamic')
     reviews_given = db.relationship('Review', foreign_keys='Review.reviewer_id', backref='reviewer', lazy='dynamic')
     reviews_received = db.relationship('Review', foreign_keys='Review.reviewee_id', backref='reviewee', lazy='dynamic')
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f"<User {self.username} ({self.email})>"
+
 
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
